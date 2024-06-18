c1ace9d60f33e2c6f3ec53076d5da99c1fea1185
Christoffer Hammarström <christoffer.hammarstrom@linuxgods.com>
2024-06-18 17:16:54 +0200

    WIP

--- src/main/java/com/linuxgods/kreiger/idea/jmte/JmteEditorNotificationProvider.java
+++ src/main/java/com/linuxgods/kreiger/idea/jmte/JmteEditorNotificationProvider.java
@@ -6,23 +6,32 @@ import com.intellij.lang.Language;
 import com.intellij.lang.html.HTMLLanguage;
 import com.intellij.lang.xml.XMLLanguage;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorBundle;
 import com.intellij.openapi.file.exclude.OverrideFileTypeManager;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.TextEditor;
 import com.intellij.openapi.fileTypes.*;
+import com.intellij.openapi.fileTypes.ex.FileTypeManagerEx;
+import com.intellij.openapi.fileTypes.impl.FileTypeAssocTable;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.*;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.templateLanguages.TemplateDataLanguageConfigurable;
+import com.intellij.psi.templateLanguages.TemplateDataLanguagePatterns;
 import com.intellij.ui.EditorNotificationPanel;
 import com.intellij.ui.EditorNotificationProvider;
 import com.intellij.ui.EditorNotifications;
+import com.linuxgods.kreiger.idea.jmte.psi.JmteFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import java.io.IOException;
@@ -36,15 +45,15 @@ import static org.jetbrains.jps.model.java.JavaResourceRootType.*;
 
 public class JmteEditorNotificationProvider implements EditorNotificationProvider {
     private static final Key<Boolean> DISABLE_NOTIFICATION = Key.create("jmte.file.type.notification.disable");
+    public static final Logger LOGGER = LoggerFactory.getLogger(JmteEditorNotificationProvider.class);
 
     @Override
     public @NotNull Function<? super @NotNull FileEditor, ? extends @Nullable JComponent> collectNotificationData(@NotNull Project project, @NotNull VirtualFile file) {
         if (PropertiesComponent.getInstance().isTrueValue(DISABLE_NOTIFICATION.toString())) {
             return CONST_NULL;
         }
+        if (file.getFileType() instanceof JmteFileType) return CONST_NULL;
         if (!(file.getFileType() instanceof LanguageFileType fileType)) return CONST_NULL;
-        Language language = fileType.getLanguage();
-        if (!potentialTemplate(language)) return CONST_NULL;
         VirtualFile parent = file.getParent();
         if (parent == null) return CONST_NULL;
         if (!underResourceRoot(project, parent)) return CONST_NULL;
@@ -63,6 +72,16 @@ public class JmteEditorNotificationProvider implements EditorNotificationProvide
             panel.setText("This file looks like a JMTE template containing "+fileType.getDisplayName());
             panel.createActionLabel("Override type for this file", () -> {
                 OverrideFileTypeManager.getInstance().addFile(file, JmteFileType.INSTANCE);
+                TemplateDataLanguagePatterns templateDataLanguagePatterns = TemplateDataLanguagePatterns.getInstance();
+                FileTypeAssocTable<Language> assocTable = templateDataLanguagePatterns.getAssocTable();
+                LOGGER.warn("assocTable {}", assocTable.findAssociatedFileType(file.getName()));
+                assocTable.addAssociation(new ExactFileNameMatcher(file.getName()), fileType.getLanguage());
+                templateDataLanguagePatterns.setAssocTable(assocTable);
+                TemplateDataLanguageConfigurable configurable = new TemplateDataLanguageConfigurable(project);
+                ShowSettingsUtil.getInstance().editConfigurable(project, configurable, () -> {
+                    configurable.selectFile(file);
+                });
+
             });
             List<VirtualFile> otherChildren = findSiblingFilesOfType(parent, file, fileType);
             if (!otherChildren.isEmpty()) {
@@ -74,15 +93,32 @@ public class JmteEditorNotificationProvider implements EditorNotificationProvide
                     }
                 });
             }
-            String ext = file.getExtension();
-            if (ext == null) {
-                return panel;
-            }
+
             FileTypeManager fileTypeManager = FileTypeManager.getInstance();
-            if (fileTypeManager.getFileTypeByExtension(ext) instanceof UnknownFileType) {
-                panel.createActionLabel("Associate '"+ext+"' extension",
-                        () -> ApplicationManager.getApplication().runWriteAction(
-                                () -> fileTypeManager.associatePattern(JmteFileType.INSTANCE, "*."+ext)));
+            fileTypeManager.getAssociations(JmteFileType.INSTANCE).forEach((FileNameMatcher association) -> {
+                if (association instanceof ExtensionFileNameMatcher extMatcher) {
+                    String ext = "."+extMatcher.getExtension();
+                    if (!file.getName().endsWith(ext)) {
+                        String newName = file.getName() + ext;
+                        panel.createActionLabel("Rename file to "+newName, () -> {
+                            try {
+                                WriteAction.run(() -> {
+                                    file.rename(JmteEditorNotificationProvider.this, newName);
+                                });
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+                    }
+                }
+            });
+            String ext = file.getExtension();
+            if (ext != null) {
+                if (fileTypeManager.getFileTypeByExtension(ext) instanceof UnknownFileType) {
+                    panel.createActionLabel("Associate '"+ext+"' extension",
+                            () -> ApplicationManager.getApplication().runWriteAction(
+                                    () -> fileTypeManager.associatePattern(JmteFileType.INSTANCE, "*."+ext)));
+                }
             }
             panel.createActionLabel(EditorBundle.message("notification.hide.message"), () -> {
                 editor.putUserData(DISABLE_NOTIFICATION, Boolean.TRUE);
